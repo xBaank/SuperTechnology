72737537022ba939362425466d497dda26bef49f
MarioGonzalezResa <mario.gonzalezresa@alumno.iesluisvives.org>
2023-02-08 11:40:45 +0100

    Preparacion del proyecto Spring + Docker PostgresSQL

---
+++ usuarios-api/src/main/kotlin/resa/rodriguez/UsuariosApiApplication.kt
@@ -0,0 +1,11 @@
+package resa.rodriguez
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UsuariosApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<UsuariosApiApplication>(*args)
+}
