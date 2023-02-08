b6a1a7895bccf3e7e5418dfdd1845f200a502a50
ALFREDO MALDONADO <alfredorafael.maldonado@alumno.iesluisvives.org>
2023-02-08 12:46:00 +0100

    Modelo de producto

--- productos-api/src/main/kotlin/productosApi/model/Producto.kt
+++ productos-api/src/main/kotlin/productosApi/model/Producto.kt
@@ -1,4 +1,44 @@
 package productosApi.model
 
-class Producto {
+import java.time.LocalDateTime
+import java.util.*
+
+data class Producto(
+    val id: UUID = UUID.randomUUID(),
+    val nombre: String,
+    val categoria: Categoria,
+    val stock: Int,
+    val description: String,
+    val createdAt: LocalDateTime,
+    val updateAt: LocalDateTime,
+    val deleteAt: LocalDateTime,
+    val precio: Double,
+    val activo: Boolean
+    ){
+    enum class Categoria(categoria: String){
+        PIEZA("PIEZA"),
+        REPARACION("REPARACION"),
+        MONTAJE("MONTAJE"),
+        PERSONALIZADO("PERSONALIZADO"),
+        MOVIL("MOVIL"),
+        PORTATIL("PORTATIL"),
+        SOBREMESA("SOBREMESA"),
+        TABLET("TABLET");
+
+        companion object {
+            fun from(categoria: String): Categoria{
+                return when ( categoria.uppercase()){
+                    "PIEZA" -> PIEZA
+                    "REPARACION" -> REPARACION
+                    "MONTAJE" -> MONTAJE
+                    "PERSONALIZADO" -> PERSONALIZADO
+                    "MOVIL" -> MOVIL
+                    "PORTATIL" -> PORTATIL
+                    "SOBREMESA" -> SOBREMESA
+                    "TABLET" -> TABLET
+                    else -> throw IllegalArgumentException("Categorías no válidas")
+                }
+            }
+        }
+    }
 }
