48fe43a8a2c7b2be0d571b1ae990d51cf1860baa
Daniel Rodríguez Muñoz <daniel.ro.mu02@gmail.com>
2023-02-10 00:06:57 +0100

    Repositorios cacheados terminados.

--- usuarios-api/src/main/kotlin/resa/rodriguez/repositories/user/UserRepositoryCached.kt
+++ usuarios-api/src/main/kotlin/resa/rodriguez/repositories/user/UserRepositoryCached.kt
@@ -7,6 +7,9 @@ import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.withContext
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.CachePut
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.PageRequest
@@ -34,10 +37,12 @@ class UserRepositoryCached
         repo.findAllByActivoOrderByCreatedAt(activo)
     }
 
+    @Cacheable("usuarios")
     override suspend fun findById(id: UUID): User? = withContext(Dispatchers.IO) {
         repo.findById(id)
     }
 
+    @Cacheable("usuarios")
     override suspend fun findByEmail(email: String): User? = withContext(Dispatchers.IO) {
         repo.findFirstByEmailContaining(email).firstOrNull()
     }
@@ -46,29 +51,71 @@ class UserRepositoryCached
         repo.findByUsernameContaining(username)
     }
 
+    @Cacheable("usuarios")
     override suspend fun findByPhone(phone: String): User? = withContext(Dispatchers.IO) {
         repo.findFirstByPhone(phone).firstOrNull()
     }
 
+    @CachePut("usuarios")
     override suspend fun save(user: User): User = withContext(Dispatchers.IO) {
         repo.save(user)
     }
 
+    @CacheEvict("usuarios")
     override suspend fun deleteById(id: UUID): User? = withContext(Dispatchers.IO) {
-        val user = repo.findById(id)
-        if (user != null) repo.deleteById(user.id)
+        val user = repo.findById(id) ?: return@withContext null
+        repo.deleteById(user.id)
         user
     }
 
-    override suspend fun setInactive(id: UUID): User? {
-        TODO("Not yet implemented")
+    @CachePut("usuarios")
+    override suspend fun setActivity(id: UUID, active: Boolean): User? = withContext(Dispatchers.IO) {
+        val user = repo.findById(id) ?: return@withContext null
+        val res = User(
+            id = user.id,
+            username = user.username,
+            email = user.email,
+            password = user.password,
+            phone = user.phone,
+            avatar = user.avatar,
+            role = user.role,
+            createdAt = user.createdAt,
+            activo = active
+        )
+        repo.save(res)
     }
 
-    override suspend fun update(id: UUID, user: User): User? {
-        TODO("Not yet implemented")
+    @CachePut("usuarios")
+    override suspend fun update(id: UUID, user: User): User? = withContext(Dispatchers.IO) {
+        val u = repo.findById(id) ?: return@withContext null
+        val res = User(
+            id = u.id,
+            username = user.username,
+            email = user.email,
+            password = user.password,
+            phone = user.phone,
+            avatar = user.avatar,
+            role = user.role,
+            createdAt = u.createdAt,
+            activo = user.activo
+        )
+        repo.save(res)
     }
 
-    override suspend fun updateCapado(id: UUID, user: User): User? {
-        TODO("Not yet implemented")
+    @CachePut("usuarios")
+    override suspend fun updateCapado(id: UUID, user: User): User? = withContext(Dispatchers.IO) {
+        val u = repo.findById(id) ?: return@withContext null
+        val res = User(
+            id = u.id,
+            username = user.username,
+            email = u.email,
+            password = user.password,
+            phone = user.phone,
+            avatar = user.avatar,
+            role = u.role,
+            createdAt = u.createdAt,
+            activo = u.activo
+        )
+        repo.save(res)
     }
 }
