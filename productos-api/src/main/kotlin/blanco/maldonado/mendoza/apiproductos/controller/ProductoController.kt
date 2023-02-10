aa602ef0c8f3f1a725054285a4fc961cc1dde249
Azahara Blanco Rodríguez <blancaluna92@hotmail.com>
2023-02-10 14:10:08 +0100

    Controlador de produtos

--- productos-api/src/main/kotlin/blanco/maldonado/mendoza/apiproductos/controller/ProductoController.kt
+++ productos-api/src/main/kotlin/blanco/maldonado/mendoza/apiproductos/controller/ProductoController.kt
@@ -1,29 +1,148 @@
 package blanco.maldonado.mendoza.apiproductos.controller
 
+import blanco.maldonado.mendoza.apiproductos.dtos.ProductosDTO
+import blanco.maldonado.mendoza.apiproductos.mappers.toDto
 import blanco.maldonado.mendoza.apiproductos.model.Producto
 import blanco.maldonado.mendoza.apiproductos.model.TestDto
 import blanco.maldonado.mendoza.apiproductos.repositories.ProductosRepository
+import jakarta.validation.Valid
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
+import java.util.UUID
 
 private val logger = KotlinLogging.logger { }
 
 @RestController
+//@RequestMapping(APIConfig.API_PATH + "/api/productos")  ento para cuando cojamos la path de el fichero config
 @RequestMapping("/api/productos")
 class ProductoController
 @Autowired constructor(
     private val repository: ProductosRepository
 ) {
-    @GetMapping("/all")
-    suspend fun findAllProductos(): ResponseEntity<Flow<Producto>> = withContext(Dispatchers.IO) {
-        logger.info { "Obteniendo todos los productos" }
-        val res = repository.findAll()
+    /**
+     * Get all : Lista de productos
+     * @return ResponseEntity<Flow<ProductosDTO>>
+     */
+    @GetMapping("")
+    suspend fun findAllProductos(): ResponseEntity<Flow<ProductosDTO>> = withContext(Dispatchers.IO) {
+        logger.info { "Get productos" }
+        val res = repository.findAll().map { it.toDto() }
         return@withContext ResponseEntity.ok(res)
+    }
+
+    /**
+     * Get {id} : producto por su id
+     * @param id : uid del producto
+     * @return ResponseEntity<ProductosDTO>
+     * @throws ResponseStatusException con el mensaje 404 si no lo encuentra
+     */
+    @GetMapping("/{id}")
+    suspend fun findProductById(@PathVariable id: UUID): ResponseEntity<ProductosDTO> = withContext(Dispatchers.IO) {
+
+        logger.info { "Obteniendo producto por id" }
+
+        try {
+            val res = repository.findById(id)?.toDto()
+            return@withContext ResponseEntity.ok(res)
+        }catch (e : ProductosNotFoundException){
+            throw ResponseStatusException(HttpStatus.NOT_FOUND,e.message)
+        }
+    }
+    /**
+     * Get {categoria} : productos por su categoria
+     * @param categoria : categoria del producto
+     * @return ResponseEntity<Flow<ProductosDTO>
+     */
+    @GetMapping("/{categoria}")
+    suspend fun findProductByCategoria(@PathVariable categoria: String): ResponseEntity<Flow<ProductosDTO>> = withContext(Dispatchers.IO) {
+        logger.info { "Get productos by categoria" }
+        val res = repository.findByCategoria(categoria).map { it.toDto() }
+        return@withContext ResponseEntity.ok(res)
+    }
+
+    /**
+     * Get {nombre} : producto por su nombre
+     * @param nombre : nombre del producto
+     * @return ResponseEntity<ProductosDTO>
+     * @throws ResponseStatusException con el mensaje 404 si no lo encuentra
+     */
+    @GetMapping("/{nombre}")
+    suspend fun findProductById(@PathVariable nombre: String): ResponseEntity<ProductosDTO> = withContext(Dispatchers.IO) {
+        logger.info { "Obteniendo producto por id" }
+
+        try {
+            val res = repository.findByNombre(nombre)?.toDto()
+            return@withContext ResponseEntity.ok(res)
+        }catch (e : ProductosNotFoundException){
+            throw ResponseStatusException(HttpStatus.NOT_FOUND,e.message)
+        }
+    }
+
+    /**
+     * Post : crea un producto
+     * @param procuctoDTO : producto
+     * @return ResponseEntity<ProductosDTO>
+     * @throws ResponseStatusException con el mensaje 400 si no es valido
+     */
+    @PostMapping("")
+    suspend fun createProduct(@Valid @RequestBody productoDto : ProductoRequestDto): ResponseEntity<ProductosDTO> = withContext(Dispatchers.IO) {
+        //todo ver si el producto exixtepor id o por nombre, y si no exixte crearlo si existe no posible
+        logger.info { "Creando un producto" }
+        try {
+            val p = productoDto.validate().toProducto()
+            val res = repository.save(p).toDto()
+            return@withContext ResponseEntity.status(HttpStatus.CREATED).body(res)
+        }catch (e:ProductoBadRequestExcepcion){
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, e.message)
+        }
+    }
+
+    /**
+     * Put : modifica un producto
+     * @param id : id producto a modificar
+     * @param producto dto : informacion del prodcuto
+     * @return ResponseEntity<ProductosDTO>
+     * @throws ResponseStatusException con el mensaje 400 si no es valido
+     */
+    @PutMapping("/{id}")
+    suspend fun updateProduct(@PathVariable id : UUID, @Valid, @RequestBody productoDto : ProductosDTO
+    ): ResponseEntity<ProductosDTO> = withContext(Dispatchers.IO) {
+        //todo ver si el producto exixte, y si no exixte que la respuesta sea not found
+        logger.info { "Modificando producto" }
+        try {
+            val p = productoDto.validate().toProducto()
+            val res = repository.update(id , p).toDto()
+            return@withContext ResponseEntity.status(HttpStatus.OK).body(res)
+        }catch (e:ProductoNotFoundExcepcion){
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, e.message)
+        }catch (e:ProductoBadRequestExcepcion){
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, e.message)
+        }
+    }
+
+    //path modificar o deshabilitar
+    @DeleteMapping("/{id}")
+    suspend fun deleteProduct(@PathVariable id : UUID): ResponseEntity<ProductosDTO> = withContext(Dispatchers.IO) {
+        //todo ver si el producto exixte, y si no not posible
+        logger.info { "Borrando producto" }
+        try {
+            repository.deleteById(id)
+            return@withContext ResponseEntity.noContent().build()
+        }catch (e: ProductoNotFoundException){
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, e.message)
+        }catch (e: ProductoConflictIntegrutyException){
+            throw ResponseStatusException(HttpStatus.CONFLICT, e.message)
+        }
+
     }
 
     @GetMapping("/test")
@@ -31,4 +150,6 @@ class ProductoController
         logger.info { "GET ALL Test" }
         return ResponseEntity.ok(listOf(TestDto("Hola : Query: $texto"), TestDto("Mundo : Query: $texto")))
     }
+
+
 }
--- productos-api/src/main/kotlin/blanco/maldonado/mendoza/apiproductos/mappers/MapperPorductos.kt
+++ productos-api/src/main/kotlin/blanco/maldonado/mendoza/apiproductos/mappers/MapperPorductos.kt
@@ -4,29 +4,17 @@ import blanco.maldonado.mendoza.apiproductos.dtos.ProductosDTO
 import blanco.maldonado.mendoza.apiproductos.model.Producto
 
 
-fun Producto.toDto(producto: Producto) = ProductosDTO(
-    id= producto.id.toString(),
-    nombre= producto.nombre,
-    categoria = producto.categoria.toString(),
-    stock = producto.stock,
-    description = producto.description,
-    createdAt= producto.createdAt.toString(),
-    updateAt = producto.updateAt.toString(),
-    deleteAt = producto.deleteAt.toString(),
-    precio = producto.precio,
-    activo = producto.activo
+fun Producto.toDto() = ProductosDTO(
+    id= this.id.toString(),
+    nombre= this.nombre,
+    categoria =this.categoria.toString(),
+    stock = this.stock,
+    description = this.description,
+    createdAt= this.createdAt.toString(),
+    updateAt = this.updateAt.toString(),
+    deleteAt = this.deleteAt.toString(),
+    precio = this.precio,
+    activo = this.activo
 )
 
-fun ProductosDTO.toProducto(productoDTO: ProductosDTO) = Producto(
-    id= productoDTO.id.toU,
-    nombre= productoDTO.nombre,
-    categoria = productoDTO.categoria,
-    stock = productoDTO.stock,
-    description = productoDTO.description,
-    createdAt= productoDTO.createdAt,
-    updateAt = productoDTO.updateAt,
-    deleteAt = productoDTO.deleteAt,
-    precio = productoDTO.precio,
-    activo = productoDTO.activo
-)
 
