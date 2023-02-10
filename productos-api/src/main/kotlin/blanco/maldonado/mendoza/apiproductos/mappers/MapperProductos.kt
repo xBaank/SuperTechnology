934776f3fb2b9e9ac659c2d748ae14f804272224
Azahara Blanco Rodríguez <blancaluna92@hotmail.com>
2023-02-10 14:31:09 +0100

    Camio en conflicto de rama por maper

--- productos-api/src/main/kotlin/blanco/maldonado/mendoza/apiproductos/mappers/MapperProductos.kt
+++ productos-api/src/main/kotlin/blanco/maldonado/mendoza/apiproductos/mappers/MapperProductos.kt
@@ -2,6 +2,9 @@ package blanco.maldonado.mendoza.apiproductos.mappers
 
 import blanco.maldonado.mendoza.apiproductos.dtos.ProductosDTO
 import blanco.maldonado.mendoza.apiproductos.model.Producto
+import java.time.LocalDateTime
+import java.util.*
+
 
 
 fun Producto.toDto() = ProductosDTO(
@@ -13,6 +16,19 @@ fun Producto.toDto() = ProductosDTO(
     createdAt= this.createdAt.toString(),
     updateAt = this.updateAt.toString(),
     deleteAt = this.deleteAt.toString(),
+    precio = this.precio,
+    activo = this.activo
+)
+
+fun ProductosDTO.toProducto() = Producto(
+    id = UUID.randomUUID(),
+    nombre = this.nombre,
+    categoria = Producto.Categoria.valueOf(this.categoria),
+    stock = this.stock,
+    description = this.description,
+    createdAt = LocalDateTime.parse(this.createdAt),
+    updateAt = LocalDateTime.parse(this.updateAt),
+    deleteAt = LocalDateTime.parse(this.deleteAt),
     precio = this.precio,
     activo = this.activo
 )
