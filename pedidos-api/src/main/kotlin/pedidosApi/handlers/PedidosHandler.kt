f0502c8c5a11969d9216121d632eceeb364c9705
Bank <elroby444@gmail.com>
2023-02-13 12:57:18 +0100

    Cleanup

--- pedidos-api/src/main/kotlin/pedidosApi/handlers/PedidosHandler.kt
+++ pedidos-api/src/main/kotlin/pedidosApi/handlers/PedidosHandler.kt
@@ -16,23 +16,25 @@ import pedidosApi.json.buildPedidoDto
 import pedidosApi.models.Pedido
 import pedidosApi.repositories.PagedFlow
 
+typealias ApplicationCallContext = PipelineContext<*, ApplicationCall>
+
 @JvmName("handleResultFlow")
-suspend fun PipelineContext<*, ApplicationCall>.handleResult(pedidoResult: Either<PedidoError, Flow<Pedido>>) =
+suspend fun ApplicationCallContext.handleResult(pedidoResult: Either<PedidoError, Flow<Pedido>>) =
     pedidoResult.fold(ifLeft = { handleError(it) }, ifRight = { call.respond(it.map(::buildPedidoDto).toList()) })
 
 @JvmName("handleResultPagedFlow")
-suspend fun PipelineContext<*, ApplicationCall>.handleResult(pedidoResult: Either<PedidoError, PagedFlow<Pedido>>) =
+suspend fun ApplicationCallContext.handleResult(pedidoResult: Either<PedidoError, PagedFlow<Pedido>>) =
     pedidoResult.fold(ifLeft = { handleError(it) }, ifRight = { call.respond(buildPagedPedidoJson(it)) })
 
 @JvmName("handleResultPedido")
-suspend fun PipelineContext<*, ApplicationCall>.handleResult(pedidoResult: Either<PedidoError, Pedido>) =
+suspend fun ApplicationCallContext.handleResult(pedidoResult: Either<PedidoError, Pedido>) =
     pedidoResult.fold(ifLeft = { handleError(it) }, ifRight = { call.respond(buildPedidoDto(it)) })
 
 @JvmName("handleResultUnit")
-suspend fun PipelineContext<*, ApplicationCall>.handleResult(pedidoResult: Either<PedidoError, Unit>) =
+suspend fun ApplicationCallContext.handleResult(pedidoResult: Either<PedidoError, Unit>) =
     pedidoResult.fold(ifLeft = { handleError(it) }, ifRight = { call.respond(HttpStatusCode.NoContent) })
 
-suspend fun PipelineContext<*, ApplicationCall>.handleError(error: PedidoError) = when (error) {
+suspend fun ApplicationCallContext.handleError(error: PedidoError) = when (error) {
     is PedidoError.PedidoNotFound -> call.respond(
         HttpStatusCode.NotFound,
         buildErrorJson(error.message, HttpStatusCode.NotFound.value)
