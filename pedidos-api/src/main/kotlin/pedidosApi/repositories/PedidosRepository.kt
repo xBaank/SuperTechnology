3d096270202e184c9cd1d7ef0cfbd463854ab243
Bank <elroby444@gmail.com>
2023-02-14 11:41:16 +0100

    Add postman endpoints

--- pedidos-api/src/main/kotlin/pedidosApi/repositories/PedidosRepository.kt
+++ pedidos-api/src/main/kotlin/pedidosApi/repositories/PedidosRepository.kt
@@ -13,12 +13,13 @@ import pedidosApi.exceptions.PedidoError.*
 import pedidosApi.extensions.toObjectIdOrNull
 import pedidosApi.models.Pedido
 
+const val MAX_SIZE = 500
+
 class PedidosRepository(client: CoroutineDatabase) {
     private val collection = client.getCollection<Pedido>()
 
     fun getByPage(page: Int, size: Int): Either<PedidoError, PagedFlow<Pedido>> {
-        if (page < 0) return InvalidPedidoPage("Page must be greater or equal than 0").left()
-        if (size < 1) return InvalidPedidoPage("Size must be greater than 0").left()
+        validatePage(page, size)
 
         val flow = collection.find().skip(page * size).limit(size).toFlow()
         return PagedFlow(page, size, flow).right()
@@ -33,8 +34,7 @@ class PedidosRepository(client: CoroutineDatabase) {
     }
 
     fun getByUserId(id: String, page: Int, size: Int): Either<PedidoError, PagedFlow<Pedido>> {
-        if (page < 0) return InvalidPedidoPage("Page must be greater or equal than 0").left()
-        if (size < 1) return InvalidPedidoPage("Size must be greater than 0").left()
+        validatePage(page, size)
 
         val flow = collection.find(Pedido::usuario / UsuarioDto::id eq id).skip(page * size).limit(size).toFlow()
         return PagedFlow(page, size, flow).right()
@@ -55,4 +55,10 @@ class PedidosRepository(client: CoroutineDatabase) {
         return Unit.right()
     }
 
+    private fun validatePage(page: Int, size: Int): Either<InvalidPedidoPage, Unit> {
+        if (page < 0) return InvalidPedidoPage("Page must be greater or equal than 0").left()
+        if (size < 1) return InvalidPedidoPage("Size must be greater than 0").left()
+        if (size >= MAX_SIZE) return InvalidPedidoPage("Size must be less or equal than $MAX_SIZE").left()
+        return Unit.right()
+    }
 }
