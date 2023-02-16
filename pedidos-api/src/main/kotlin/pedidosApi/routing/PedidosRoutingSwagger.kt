e0ad14d736a04e044c7397cc4ee3a24052168eb7
Iván Azagra <ivanazagratroya@gmail.com>
2023-02-16 12:06:20 +0100

    swagger post, put, delete

--- pedidos-api/src/main/kotlin/pedidosApi/routing/PedidosRoutingSwagger.kt
+++ pedidos-api/src/main/kotlin/pedidosApi/routing/PedidosRoutingSwagger.kt
@@ -2,7 +2,9 @@ package pedidosApi.routing
 
 import io.github.smiley4.ktorswaggerui.dsl.OpenApiRoute
 import io.ktor.http.*
+import pedidosApi.dto.requests.CreatePedidoDto
 import pedidosApi.dto.responses.PagedFlowDto
+import pedidosApi.dto.responses.PedidoDto
 import pedidosApi.models.Pedido
 
 fun OpenApiRoute.getAll() {
@@ -21,6 +23,54 @@ fun OpenApiRoute.getAll() {
         HttpStatusCode.OK to {
             description = "Paged result"
             body<PagedFlowDto<Pedido>>()
+        }
+    }
+}
+
+fun OpenApiRoute.post() {
+    description = "Updates or inserts pedidos"
+    request {
+        queryParameter<CreatePedidoDto>("pedido") {
+            description = "CreatePedidoDto that will be inserted"
+            required = true
+        }
+    }
+    response {
+        HttpStatusCode.Created to {
+            description = "Pedido inserted"
+            body<PedidoDto>()
+        }
+    }
+}
+
+fun OpenApiRoute.put() {
+    description = "Inserts or updates pedidos with given id"
+    request {
+        queryParameter<String>("id") {
+            description = "Id from the pedido"
+            required = true
+        }
+    }
+    response {
+        HttpStatusCode.Created to {
+            description = "Pedido saved"
+            body<PedidoDto>()
+        }
+    }
+}
+
+fun OpenApiRoute.delete() {
+    description = "Deletes pedidos with given id"
+    request {
+        queryParameter<String>("id") {
+            description = "Id from the pedido"
+            required = true
+        }
+    }
+    response {
+        HttpStatusCode.OK to {
+            description = "Pedido deleted"
+            body<Unit>()
         }
     }
 }
