40d980b8607d7ca46f6fa131bcb4841dc3b0ae18
Bank <elroby444@gmail.com>
2023-02-21 11:47:10 +0100

    Cleanup

--- pedidos-api/src/test/kotlin/integration/JsonExtensions.kt
+++
@@ -1,7 +0,0 @@
-package integration
-
-import io.ktor.client.statement.*
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
-
-suspend inline fun <reified T> HttpResponse.bodyAs() = Json.decodeFromString<T>(bodyAsText())
--- pedidos-api/src/test/kotlin/integration/routing/PedidosRoutingTest.kt
+++ pedidos-api/src/test/kotlin/integration/routing/PedidosRoutingTest.kt
@@ -1,45 +1,45 @@
 package integration.routing
 
-import integration.bodyAs
 import integration.data.PedidosData
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.testing.*
-import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import org.amshove.kluent.shouldBe
 import org.amshove.kluent.shouldBeEqualTo
-import org.amshove.kluent.shouldNotBeNull
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.koin.core.context.stopKoin
 import org.testcontainers.containers.MongoDBContainer
 import org.testcontainers.utility.DockerImageName
 import pedidosApi.dto.responses.ErrorDto
 import pedidosApi.dto.responses.PagedFlowDto
 import pedidosApi.dto.responses.PedidoDto
+import pedidosApi.models.EstadoPedido
 
 
-private val ApplicationTestBuilder.jsonClient: HttpClient
-    get() {
-        val jsonClient = createClient {
-            install(ContentNegotiation) {
-                json(Json { ignoreUnknownKeys = true })
-            }
-        }
-        return jsonClient
-    }
-
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class PedidosRoutingTest {
     @JvmField
     val mongoDBContainer = MongoDBContainer(DockerImageName.parse("mongo:latest")).apply {
         start()
     }
 
+    lateinit var jsonClient: HttpClient
+
+    private val ApplicationTestBuilder.jsonClient: HttpClient
+        get() {
+            val jsonClient = createClient {
+                install(ContentNegotiation) {
+                    json(Json { ignoreUnknownKeys = true })
+                }
+            }
+            return jsonClient
+        }
 
     init {
         stopKoin()
@@ -57,29 +57,41 @@ class PedidosRoutingTest {
 
         val response = jsonClient.get("/pedidos")
 
-        val result = response.bodyAs<PagedFlowDto<PedidoDto>>()
+        val result = response.body<PagedFlowDto<PedidoDto>>()
 
         responsePost.status.shouldBe(HttpStatusCode.Created)
         response.status.shouldBe(HttpStatusCode.OK)
         result.size.shouldBeEqualTo(1)
+        result.result.first().iva.shouldBeEqualTo(PedidosData.createPedido.iva)
+        result.result.first().tareas.size.shouldBeEqualTo(PedidosData.createPedido.tareas.size)
     }
 
     @Test
     fun `should get all pedidos by page`() = testApplication {
         val response = jsonClient.get("/pedidos?page=0&size=10")
         response.status.shouldBe(HttpStatusCode.OK)
+
+        val result = response.body<PagedFlowDto<PedidoDto>>()
+        result.size.shouldBeEqualTo(0) //Real size, not the one specified in the request
+        result.page.shouldBeEqualTo(0)
     }
 
     @Test
     fun `should not get all pedidos by page`() = testApplication {
         val response = jsonClient.get("/pedidos?page=-5&size=10")
         response.status.shouldBe(HttpStatusCode.BadRequest)
+
+        val result = response.body<ErrorDto>()
+        result.code.shouldBeEqualTo(400)
     }
 
     @Test
     fun `should not get by incorrect id`() = testApplication {
         val response = jsonClient.get("/pedidos/fad")
         response.status.shouldBe(HttpStatusCode.BadRequest)
+
+        val result = response.body<ErrorDto>()
+        result.code.shouldBeEqualTo(400)
     }
 
     @Test
@@ -87,8 +99,11 @@ class PedidosRoutingTest {
         val response = jsonClient.put("/pedidos/fad") {
             contentType(ContentType.Application.Json)
         }
+
+        val result = response.body<ErrorDto>()
+
         response.status.shouldBe(HttpStatusCode.BadRequest)
-        response.body<ErrorDto>().shouldNotBeNull()
+        result.code.shouldBeEqualTo(400)
     }
 
     @Test
@@ -97,7 +112,12 @@ class PedidosRoutingTest {
             contentType(ContentType.Application.Json)
             setBody(PedidosData.createPedido)
         }
+
+        val result = response.body<PedidoDto>()
+
         response.status.shouldBe(HttpStatusCode.Created)
+        result.iva.shouldBeEqualTo(PedidosData.createPedido.iva)
+        result.tareas.size.shouldBeEqualTo(PedidosData.createPedido.tareas.size)
     }
 
     @Test
@@ -106,7 +126,11 @@ class PedidosRoutingTest {
             contentType(ContentType.Application.Json)
             setBody(PedidosData.incorrectFormat)
         }
+
+        val result = response.body<ErrorDto>()
+
         response.status.shouldBe(HttpStatusCode.BadRequest)
+        result.code.shouldBeEqualTo(400)
     }
 
     @Test
@@ -116,15 +140,21 @@ class PedidosRoutingTest {
             setBody(PedidosData.createPedido)
         }
 
-        val pedidoCreated = Json.decodeFromString<PedidoDto>(responsePost.bodyAsText())
+        val pedidoCreated = responsePost.body<PedidoDto>()
 
         val responsePut = jsonClient.put("/pedidos/${pedidoCreated.id}") {
             contentType(ContentType.Application.Json)
             setBody(PedidosData.updatePedido)
         }
 
+        val pedidoUpdated = responsePut.body<PedidoDto>()
+
         responsePost.status.shouldBe(HttpStatusCode.Created)
         responsePut.status.shouldBe(HttpStatusCode.OK)
+        pedidoCreated.iva.shouldBeEqualTo(PedidosData.createPedido.iva)
+        pedidoCreated.tareas.size.shouldBeEqualTo(PedidosData.createPedido.tareas.size)
+        pedidoUpdated.iva.shouldBeEqualTo(PedidosData.updatePedido.iva)
+        pedidoUpdated.estado.shouldBeEqualTo(EstadoPedido.EN_PROCESO)
     }
 
     @Test
@@ -134,12 +164,15 @@ class PedidosRoutingTest {
             setBody(PedidosData.createPedido)
         }
 
-        val pedidoCreated = Json.decodeFromString<PedidoDto>(responsePost.bodyAsText())
+        val pedidoCreated = responsePost.body<PedidoDto>()
 
-        val responsePut = jsonClient.get("/pedidos/${pedidoCreated.id}")
+        val responseGet = jsonClient.get("/pedidos/${pedidoCreated.id}")
+
+        val pedidoFound = responseGet.body<PedidoDto>()
 
         responsePost.status.shouldBe(HttpStatusCode.Created)
-        responsePut.status.shouldBe(HttpStatusCode.OK)
+        responseGet.status.shouldBe(HttpStatusCode.OK)
+        pedidoCreated.shouldBeEqualTo(pedidoFound)
     }
 
     @Test
@@ -149,12 +182,15 @@ class PedidosRoutingTest {
             setBody(PedidosData.createPedido)
         }
 
-        val pedidoCreated = Json.decodeFromString<PedidoDto>(responsePost.bodyAsText())
+        val pedidoCreated = responsePost.body<PedidoDto>()
 
         val responseGet = jsonClient.get("/pedidos/usuario/${pedidoCreated.usuario.id}")
 
+        val pedidosFound = responseGet.body<PagedFlowDto<PedidoDto>>()
+
         responsePost.status.shouldBe(HttpStatusCode.Created)
         responseGet.status.shouldBe(HttpStatusCode.OK)
+        pedidoCreated.shouldBeEqualTo(pedidosFound.result.first())
     }
 
     @Test
@@ -164,12 +200,15 @@ class PedidosRoutingTest {
             setBody(PedidosData.createPedido)
         }
 
-        val pedidoCreated = Json.decodeFromString<PedidoDto>(responsePost.bodyAsText())
+        val pedidoCreated = responsePost.body<PedidoDto>()
 
         val responseGet = jsonClient.get("/pedidos/usuario/${pedidoCreated.usuario.id}?page=-5&size=10")
 
+        val pedidoFound = responseGet.body<ErrorDto>()
+
         responsePost.status.shouldBe(HttpStatusCode.Created)
         responseGet.status.shouldBe(HttpStatusCode.BadRequest)
+        pedidoFound.code.shouldBeEqualTo(400)
     }
 
     @Test
@@ -177,7 +216,9 @@ class PedidosRoutingTest {
         val responsePut = jsonClient.put("/pedidos/63ebb2569be16967bba54c3b") {
             contentType(ContentType.Application.Json)
         }
+        val result = responsePut.body<ErrorDto>()
         responsePut.status.shouldBe(HttpStatusCode.NotFound)
+        result.code.shouldBeEqualTo(404)
     }
 
     @Test
@@ -186,12 +227,14 @@ class PedidosRoutingTest {
             contentType(ContentType.Application.Json)
             setBody(PedidosData.createPedido)
         }
-        val pedidoCreated = Json.decodeFromString<PedidoDto>(responsePost.bodyAsText())
+
+        val pedidoCreated = responsePost.body<PedidoDto>()
 
         val responseDelete = jsonClient.delete("/pedidos/${pedidoCreated.id}")
 
         responsePost.status.shouldBe(HttpStatusCode.Created)
         responseDelete.status.shouldBe(HttpStatusCode.NoContent)
+        pedidoCreated.iva.shouldBeEqualTo(PedidosData.createPedido.iva)
     }
 
     @Test
