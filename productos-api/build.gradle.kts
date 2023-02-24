c64a78ef7c7091e9ddfb71d11a0ddbf276836170
Azahara Blanco Rodríguez <blancaluna92@hotmail.com>
2023-02-24 15:51:45 +0100

    mergeo de version final test controlador

--- productos-api/build.gradle.kts
+++ productos-api/build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-security")
+   // implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
