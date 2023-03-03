179556c5503b3022adebc5978a3c9704e78814e1
Mario <mariogr901@gmail.com>
2023-03-03 10:43:16 +0100

    Certificados desplazados a resources // La clave jwt escondida en propiedades

--- usuarios-api/src/main/kotlin/resa/rodriguez/config/security/jwt/JwtTokensUtils.kt
+++ usuarios-api/src/main/kotlin/resa/rodriguez/config/security/jwt/JwtTokensUtils.kt
@@ -3,11 +3,10 @@ package resa.rodriguez.config.security.jwt
 import com.auth0.jwt.algorithms.Algorithm
 import com.auth0.jwt.JWT
 import com.auth0.jwt.interfaces.DecodedJWT
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import resa.rodriguez.models.User
 import java.util.*
-
-private val algorithm: Algorithm = Algorithm.HMAC512("Zanahoria turbopropulsada")
 
 /**
  * Contiene los métodos necesarios para la creación de tokens, comprobación y decodificación para verificarlos
@@ -15,6 +14,10 @@ private val algorithm: Algorithm = Algorithm.HMAC512("Zanahoria turbopropulsada"
  */
 @Component
 class JwtTokensUtils {
+
+    @Value("\${jwt.secret}")
+    private val algorithmSecret: String? = null
+
     fun create(user: User): String {
         return JWT.create()
             .withSubject(user.id.toString())
@@ -25,11 +28,11 @@ class JwtTokensUtils {
             .withClaim("role", user.role.name)
             .withClaim("active", user.active)
             .withExpiresAt(Date(System.currentTimeMillis() + (24 * 60 * 60 * 1_000)))
-            .sign(algorithm)
+            .sign(Algorithm.HMAC512(algorithmSecret))
     }
 
     fun decode(token: String): DecodedJWT? {
-        val verifier = JWT.require(algorithm)
+        val verifier = JWT.require(Algorithm.HMAC512(algorithmSecret))
             .build()
 
         return try {
